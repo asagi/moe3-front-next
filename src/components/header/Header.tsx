f55625b485b1188bb1fd0cfa45dc1a1ebe36a4b1
Keiju Asagi <k.asagi@nifty.com>
2023-01-29 01:03:20 +0900

    fix(auth): code cleanup (#21)
    Co-authored-by: Keiju Asagi <asagi@tepidworks.jp>

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,15 @@
-import { useEffect, useRef } from 'react';
 import SignoutButton from './LogoutButton';
 import TwitterLoginButton from './TwitterLoginButton';
 import { useAuthContext } from '~/feature/auth/AuthProvider';
 
 export const Header = () => {
-  const initialized = useRef<boolean>(false);
-
   const { user } = useAuthContext();
-  initialized.current = user !== undefined;
 
   const button = (() => {
     // logged in
-    if (user) return <SignoutButton />;
+    if (user != null) return <SignoutButton />;
     // logged out
-    if (initialized.current) return <TwitterLoginButton />;
+    if (user === null) return <TwitterLoginButton />;
     // now loading ...
     return <></>;
   })();
