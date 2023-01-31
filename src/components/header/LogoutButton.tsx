d31eaa867dd42bbab89023071d8c286007220be3
Keiju Asagi <asagi@tepidworks.jp>
2023-01-31 22:43:47 +0900

    feat(auth): add logged out page

--- src/components/header/LogoutButton.tsx
+++ src/components/header/LogoutButton.tsx
@@ -1,11 +1,15 @@
+'use client';
+
 import { getAuth, signOut } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
 
 export const LogoutButton = () => {
+  const router = useRouter();
   const clickButton = async () => {
     const auth = getAuth();
     try {
       await signOut(auth);
-      // Sign-out successful.
+      router.push('/logout');
     } catch (error) {
       // An error happened.
       console.error(error);
